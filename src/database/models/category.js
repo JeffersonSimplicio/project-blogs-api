06c31adc6e443ae84fa23ee7f976101dfe710cc6
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-08-25 20:03:40 -0300

    Req07: OK; criado model de Categories

---
+++ src/database/models/category.js
@@ -0,0 +1,17 @@
+const Category = (sequelize, DataTypes) => {
+  const Category = sequelize.define("Category", {
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+    },
+    name: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+  });
+
+  return Category;
+};
+
+module.exports = Category;
