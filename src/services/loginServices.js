6365918b75ae2021f57bfc5c7ea562218d411922
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-08-26 13:34:44 -0300

    Req 03: WIP; feat: caso usuairo tudo OK, gera e roterna um token valido por 1 dia

---
+++ src/services/loginServices.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const { User } = require('../database/models');
+
+async function login({ email, password }) {
+  const user = await User.findOne({ where: { email } });
+
+  if (!user || user.password !== password) {
+    return { message: 'Invalid fields' };
+  }
+
+  const secret = process.env.JWT_SECRET || 'suaSenhaSecreta';
+  const jwtConfig = {
+    expiresIn: '1d',
+    algorithm: 'HS256',
+  };
+  const token = jwt.sign({ data: user }, secret, jwtConfig);
+
+  return { token };
+}
+
+module.exports = {
+  login,
+};
