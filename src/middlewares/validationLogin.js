cb1275fa035591e5c331c9540c69b408c1106df1
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-08-26 18:03:59 -0300

    recactor: remoção delinhas desnecessarias

--- src/middlewares/validationLogin.js
+++ src/middlewares/validationLogin.js
@@ -1,12 +1,12 @@
 const { validator, schemaLogin } = require('../utils/schema');
 
 function validationLogin(req, res, next) {
-  console.log('Entrou na validação');
   const { email, password } = req.body;
+  
   const result = validator(schemaLogin, { email, password });
-  if (result.message) {
-    return res.status(400).json(result);
-  }
+
+  if (result.message) return res.status(400).json(result);
+  
   next();
 }
 
