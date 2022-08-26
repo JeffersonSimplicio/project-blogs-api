7031cd3d3d0fbf29d44bba97fcad4dfca65e350a
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-08-26 13:01:32 -0300

    Req 03: WIP; feat: service verifica se usuario existe e se a senha esta correta, em caso de erro retorna uma mensagem e code 400

--- src/services/login.js
+++ src/services/login.js
@@ -4,12 +4,12 @@ const { User } = require('../database/models');
 async function login({ email, password }) {
   const user = await User.findOne({ where: { email } });
 
-  // if (!user || user.password !== password) {
-  //   return {
-  //     code: 400,
-  //     message: { message: 'Invalid fields' },
-  //   };
-  // }
+  if (!user || user.password !== password) {
+    return {
+      code: 400,
+      message: { message: 'Invalid fields' },
+    };
+  }
 }
 
 module.exports = {
