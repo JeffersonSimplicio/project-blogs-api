74c5a28847d0d0a8982beaa26a43609ecb581968
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-08-25 21:05:13 -0300

    Req10: OK; feat: criado model de BlogPosts

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,30 @@
+const BlogPost = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define("BlogPost", {
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+    },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: {
+      foreignKey: true,
+      type: DataTypes.INTEGER,
+    },
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  },
+  {
+    timestamps: false,
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User,
+      { foreignKey: 'userId', as: 'user' });
+  };
+
+  return BlogPost;
+
+};
+
+module.exports = BlogPost;
