671879a63bd422b4c5cad5f521a9b38fa3e5f853
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-08-26 18:24:09 -0300

    Req04: WIP; feat: criado service responsavel por criar um novo usuario

---
+++ src/services/userServices.js
@@ -0,0 +1,27 @@
+// const jwt = require('jsonwebtoken');
+const { User } = require('../database/models');
+
+async function create({ displayName, email, password, image }) {
+  const [user, created] = await User.findOrCreate({
+    where: { email },
+    defaults: {
+      displayName,
+      email,
+      password,
+      image,
+    },
+  });
+
+  console.log('user: ', user);
+  console.log('created: ', created);
+
+  if (!created) {
+    return { message: 'User already registered' };
+  }
+
+  return user;
+}
+
+module.exports = {
+  create,
+};
