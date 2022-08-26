87d7d7f2c5ca9093c35e9309de7c20987fac7e02
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-08-26 18:25:10 -0300

    Req04: WIP; feat: criado middleware para validação de token

---
+++ src/middlewares/auth/validateJWT.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const { User } = require('../../database/models');
+
+async function validateJWT(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (token) return res.status(401).json({ message: 'Token not found' });
+
+  try { 
+    const secret = process.env.JWT_SECRET || 'suaSenhaSecreta';
+    const decoded = jwt.verify(token, secret);
+    console.log('decoded: ', decoded);
+
+    const user = await User.findOne({
+      where: { email: decoded.email, displayName: decoded.user },
+    });
+
+    if (!user) return res.status(401).json({ message: 'Expired or invalid token' });
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+}
+
+module.exports = {
+  validateJWT,
+};
