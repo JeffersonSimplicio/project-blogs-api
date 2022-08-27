8ccf0593d1f72108dfc54ac9b211830788467ced
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-08-27 13:42:01 -0300

    Req07: WIP; função create do controller

--- src/controller/categoryController.js
+++ src/controller/categoryController.js
@@ -1,3 +1,15 @@
 const categoryServices = require('../services/categoryServices');
 
-module.exports = {};
+async function create(req, res) {
+  const { name } = req.body;
+
+  const result = await categoryServices.create(name);
+  if (result.message) {
+    return res.status(409).json(result);
+  }
+  res.status(201).json(result);
+}
+
+module.exports = {
+  create,
+};
