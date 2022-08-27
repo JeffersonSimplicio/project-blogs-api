a889ac20dcb5b4c98bd3e61c56f2353aaf57fad8
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-08-27 11:35:51 -0300

    Req04: WIP; refactor: criado service responsavel por user, criada função create

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,24 +1,18 @@
+const tokenGenerator = require('../utils/tokenGenerator');
 const { User } = require('../database/models');
 
 async function create({ displayName, email, password, image }) {
   const [user, created] = await User.findOrCreate({
     where: { email },
-    defaults: {
-      displayName,
-      email,
-      password,
-      image,
-    },
+    defaults: { displayName, email, password, image },
   });
-
-  console.log('user: ', user);
-  console.log('created: ', created);
 
   if (!created) {
     return { message: 'User already registered' };
   }
 
-  return user;
+  const token = tokenGenerator({ email: user.email, user: user.displayName });
+  return token;
 }
 
 module.exports = {
