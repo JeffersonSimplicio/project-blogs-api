8bb5c00f3186b96beca165ecd2310a142a6faa9d
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-08-27 13:34:41 -0300

    Req07: WIP; criada rota para adição de nova categoria

---
+++ src/routes/categoryRouter.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const router = express.Router();
+
+const rescue = require('../utils/rescue');
+
+const validateJWT = require('../middlewares/auth/validateJWT');
+const validation = require('../middlewares/validationCategory');
+const categoryController = require('../controller/categoryController');
+
+router.use(validateJWT);
+
+router.post(
+  '/',
+  rescue(validation.validationCategory),
+  rescue(categoryController.create),
+);
+
+module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const loginRoute = require('./loginRouter');
 const userRoute = require('./userRouter');
+const categoryRoute = require('./categoryRouter');
 
 module.exports = {
   loginRoute,
   userRoute,
+  categoryRoute,
 };
