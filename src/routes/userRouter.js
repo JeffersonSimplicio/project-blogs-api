509e0a9165546cb2c5d6b22057ba5894f5f1b53d
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-08-27 12:10:09 -0300

    Req05: OK; feat: criada rota para gelAll

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const rescue = require('../utils/rescue');
 
-// const validateJWT = require('../middlewares/auth/validateJWT');
+const validateJWT = require('../middlewares/auth/validateJWT');
 const validation = require('../middlewares/validationUser');
 const userController = require('../controller/userController');
 
@@ -13,5 +13,9 @@ router.post(
   rescue(validation.validationUser),
   rescue(userController.create),
 );
+
+router.use(validateJWT);
+
+router.get('/', userController.getAll);
 
 module.exports = router;
