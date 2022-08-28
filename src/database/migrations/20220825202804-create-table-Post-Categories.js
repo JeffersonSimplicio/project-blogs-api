c3f13c5a540f05fca9ae67872430a06c6750b47f
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-08-28 15:49:55 -0300

    Req12: WIP; fix: referencia a talbela corrigido

--- src/database/migrations/20220825202804-create-table-Post-Categories.js
+++ src/database/migrations/20220825202804-create-table-Post-Categories.js
@@ -10,7 +10,7 @@ module.exports = {
         onUpdate: 'CASCADE',
         onDelete: 'CASCADE',
         references: {
-          model: 'Users',
+          model: 'BlogPosts',
           key: 'id',
         },
       },
