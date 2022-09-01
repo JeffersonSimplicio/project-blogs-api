2be98994b648b8dff185e27912ef0febb6561789
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-09-01 16:26:37 -0300

    fix(seeders): senhas das seeders criptografadas -as senhas das seeders passam a ser criptografadas para que o padrões também possam ser usados; -as senhas 'reais' estão comendas na seeders

--- src/database/seeders/20200812183211-Users.js
+++ src/database/seeders/20200812183211-Users.js
@@ -5,14 +5,16 @@ module.exports = {
         id: 1,
         displayName: 'Lewis Hamilton',
         email: 'lewishamilton@gmail.com',
-        password: '123456',
+        password: '$2b$10$5Zm8ESrBXgHbOidbqu.Y1eezWKHGUpero.oV/fcKaUWHrGDf1sm1.',
+        // password: '123456',
         image: 'https://upload.wikimedia.org/wikipedia/commons/1/18/Lewis_Hamilton_2016_Malaysia_2.jpg',
       },
       {
         id: 2,
         displayName: 'Michael Schumacher',
         email: 'MichaelSchumacher@gmail.com',
-        password: '123456',
+        password: '$2b$10$h0aleHPj93VggAYfLMC.Nu0ZBtU2aAPgbB7VlqKVsuoO9EHVXF/mm',
+        // password: '123456',
         image: 'https://sportbuzz.uol.com.br/media/_versions/gettyimages-52491565_widelg.jpg',
       },
       ], { timestamps: false });
