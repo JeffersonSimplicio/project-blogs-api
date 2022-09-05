14512db4a083b4a2eb86db461de71be1f783d807
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-09-05 10:15:31 -0300

    chore: instalação da lib cors - lib instalada para possibilitar o uso do serviço no navegador

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes/index');
 const errorHandling = require('./middlewares/error/error');
 
@@ -6,6 +7,7 @@ const errorHandling = require('./middlewares/error/error');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 // ...
