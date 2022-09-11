8868639129c0c45529b5258bad5fd1ee305a83f7
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-09-10 21:55:32 -0300

    feat: Rota para verificar servidor - a rota '/', agora retorna uma mensagem e uma imagem

--- src/server.js
+++ src/server.js
@@ -5,8 +5,10 @@ const app = require('./api');
 const port = process.env.API_PORT || 3000;
 
 // não remova esse endpoint
+const urlImage = 'https://www.intoxianime.com/wp-content/uploads/2022/06/s2.jpg';
 app.get('/', (_request, response) => {
-  response.send();
+  response.send(`<H1>The server is online.</H1>
+  <img src="${urlImage}" alt="Ushio Kofune like" style="width:500px">`);
 });
 
 app.listen(port, () => console.log('ouvindo porta', port));
