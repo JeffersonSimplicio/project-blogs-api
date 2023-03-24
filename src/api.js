19cbae3660ff5a847ff372c18690583bd327d939
JeffersonSimplicio <jeffersonsimplicio.js@gmail.com>
2023-03-24 11:18:23 -0300

    docs: create user successful

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
 const routes = require('./routes/index');
 const errorHandling = require('./middlewares/error/error');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 
@@ -12,6 +14,7 @@ app.use('/login', routes.loginRoute);
 app.use('/user', routes.userRoute);
 app.use('/categories', routes.categoryRoute);
 app.use('/post', routes.postRoute);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(errorHandling);
 
 // É importante exportar a constante `app`,
